e055cba65f5a21c572acb25fcd3935f4bdaa9f66
OksanaNan <oksana.semka@gmail.com>
2023-06-12 13:33:34 +0300

    first commit

---
+++ react-tutorial/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <nav>
+     <h1>Bob's Bistro</h1>
+     <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+     </ul>
+    </nav>
+  )
+}
+
+export default App
